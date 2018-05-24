52eae3afd71e7ea465bd32ce20b808a90e202ac8
Þormóður Dagsson <thormodurdagsson@ormours-iMac.local>
2018-05-24 10:13:15 +0000

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  MAPBOX: {
-    apiKey: "..."
-  }
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiZGFnc3NvbiIsImEiOiJjajk0MTRqdWIzZGxwMzNycGtreDhxMmRxIn0.0zk_7FSvF_LlQ0AD2cChWQ'
 };
